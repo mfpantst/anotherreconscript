2f5233631f761edc3b98e66cfcd231598bd12da2
mfpantst <59129510+mfpantst@users.noreply.github.com>
2020-02-28 20:21:26 -0500

    Add files via upload

---
+++ config.sh
@@ -0,0 +1,22 @@
+
+#Path Variables
+
+input="$HOME""/anotherreconscript/domains.txt"
+output="$HOME""/anotherreconscript/output"
+sublist3rpath="$HOME""/Sublist3r"
+dirsearchpath="$HOME""/dirsearch"
+arjunPATH="$HOME""/Arjun"
+payloadspath="$HOME""/anotherreconscript/payloads.txt"
+xsstrikepath="$HOME""/XSStrike"
+
+#Control switches for individual processes
+sublist3r="ON"
+httprobe="ON"
+dirsearch="ON"
+aquatone="ON"
+arjun="ON"
+
+runxss="OFF"
+XSSCONFIG="ON"
+XSSPAYLOAD="OFF"
+XSSTRIKE="OFF"
